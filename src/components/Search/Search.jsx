a147df934ec4ffabee2ec56cccebbce1c2cbce07
Julianne Johnson <tweetypie417@yahoo.com>
2018-11-29 21:53:08 -0600

    added custom radio buttons to search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,7 +3,17 @@ import './Search.css';
 
 const Search = () => (
   <div className="search-container">
-    <p>Search Component</p>
+    <p className="title">What are you searching for?</p>
+    <div className="inputs-container">
+      <label>People
+        <input type="radio" checked={true} name="radio"/>
+        <span className="btn-radio"></span>
+      </label>
+      <label>Movies
+        <input type="radio" name="radio"/>
+        <span className="btn-radio"></span>
+      </label>
+    </div>
   </div>
 )
 
